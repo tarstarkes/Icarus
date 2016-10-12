a144bb436617c8f2c2eaf2ece2cbc79522527c62
Connar Stone <tarstarkes@hotmail.com>
2016-10-12 11:14:58 -0700

    added some article functionality
    still needs the article list to be added.

--- main/home/views.py
+++ main/home/views.py
@@ -6,5 +6,7 @@ from home.models import *
 
 def index(request):
 	project_list = ProjectdbProject.objects.filter(publish=True).filter(Q(status_id=1) | Q(status_id=2)).order_by("-fiscalyear")[:10]
-	year_list = PublicationsRipples.objects.all().distinct('year').order_by('-year')
-	return render(request, 'home/index.html', {'projects': project_list, 'years': year_list})
+	year_list = PublicationsRipples.objects.all().distinct('year').order_by('-year').values('year')
+	ripple_list = PublicationsRipples.objects.all()
+	edition_list = PublicationsRipplesedition.objects.all().order_by('-year')
+	return render(request, 'home/index.html', {'projects': project_list, 'years': year_list, 'ripples': ripple_list, 'edition': edition_list})
