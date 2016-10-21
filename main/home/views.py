dd830f01f83326abd5687800cb661c1233f35ab6
Connar Stone <tarstarkes@hotmail.com>
2016-10-20 17:14:34 -0700

    lot of work done on main page
    footer still needs work

--- main/home/views.py
+++ main/home/views.py
@@ -9,7 +9,6 @@ from django.db.models import F
 def index(request):
 	project_list = ProjectdbProject.objects.filter(publish=True).filter(Q(status_id=1) | Q(status_id=2)).order_by("-fiscalyear")[:10]
 	year_list = Ripples.objects.all().distinct('year').order_by('-year').values('year')
-	ripple_list = Ripples.objects.all()
-	ripple_query = Ripplesarticle.objects.filter(ripples_id=F('ripples_id_id'))
-	ripples = serializers.serialize('json', ripple_query)
-	return render(request, 'home/index.html', {'projects': project_list, 'years': year_list, 'ripples': ripples})
+	ripple_list = serializers.serialize('json', Ripples.objects.all().order_by('-year'))
+	ripple_article = serializers.serialize('json', Ripplesarticle.objects.all())
+	return render(request, 'home/index.html', {'projects': project_list, 'years': year_list, 'ripples': ripple_list, 'articles': ripple_article})
